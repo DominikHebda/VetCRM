49aa2f9a75c9585d57a41ee33685c8e460c4b8e3
DominikHebda <hebdad@wp.pl>
2024-12-29 20:53:44 +0100

    added function: add_pet

--- Database/operations_pets.py
+++ Database/operations_pets.py
@@ -20,3 +20,34 @@ def fetch_pets():
             connection.close()
 
 fetch_pets()
+
+def add_pet(pet_name, species, breed, age):
+    try:
+        connection = create_connection()
+        if connection:
+            cursor = connection.cursor()
+            query = """
+            INSERT INTO pets (pet_name, species, breed, age)
+            VALUES (%s, %s, %s, %s)
+            """
+            cursor.execute(query, (pet_name, species, breed, age))
+            connection.commit()
+            print(f"Zwierzę {pet_name} zostało dodane")
+            cursor.close()
+            connection.close()
+    except Exception as e:
+        print(f"Błąd podczas dodawania zwierzęcia {e}")
+    finally:
+        if connection.is_connected():
+            cursor.close()
+            connection.close()
+
+def add_pet_data():
+    pet_name = input("Podaj nazwę zwierzęcia: ")
+    species = input("Podaj gatunek zawierzęcia: ")
+    breed = input("Podaj rasę zwierzęcia: ")
+    age = input("Podaj wiek zwierzęcia: ")
+
+    add_pet(pet_name, species, breed, age)
+
+add_pet_data()
