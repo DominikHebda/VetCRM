66527a9fe66a74ef39415262aa95f76822817346
DominikHebda <hebdad@wp.pl>
2024-12-29 20:24:11 +0100

    added function: soft_delete_doctor

--- Database/operations_doctors.py
+++ Database/operations_doctors.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from Database.connection import create_connection
 
 def fetch_doctors():
@@ -38,9 +39,9 @@ def add_doctor_data():
     last_name = input("Podaj nazwisko lekarza: ")
     specialization = input("Podaj specjalizacje lekarza: ")
 
-    # add_doctor(first_name, last_name, specialization)
+    add_doctor(first_name, last_name, specialization)
 
-# add_doctor_data()
+add_doctor_data()
 
 def find_doctor():
     try:
@@ -67,7 +68,7 @@ def find_doctor():
             cursor.close()
             connection.close()
 
-# find_doctor()
+find_doctor()
 
 def update_doctor():
     doctors = find_doctor()
@@ -119,3 +120,48 @@ def update_doctor():
         print(f"Błąd podczas aktualizowania danych lekarza")
 
 update_doctor()
+
+def soft_delete_doctor():
+    doctors = find_doctor()
+    if not doctors:
+        return
+    
+    try:
+        doctor_id = int(input("Podaj ID doktora, którego chcesz usunąć (soft delete): "))
+        selected_doctor = None
+        for doctor in doctors:
+            if doctor[0] == doctor_id:
+                selected_doctor = doctor
+                break
+        if not selected_doctor:
+            print(f"Nie znaleziono doktora o ID {doctor_id}")
+            return
+        print(f"Wybrano klienta: {selected_doctor[1]} {selected_doctor[2]} (ID: {selected_doctor[0]})")
+        current_time = datetime.now()
+    
+        connection = create_connection()
+        cursor = connection.cursor()
+
+        query = """
+        UPDATE doctors 
+        SET soft_delete = %s
+        WHERE iddoctor = %s AND soft_delete IS NULL
+        """
+        cursor.execute(query, (current_time, doctor_id))
+        connection.commit()
+
+        if cursor.rowcount > 0:
+            print(f"Doktor o ID {doctor_id} został oznaczony jako usunięty.")
+        else:
+            print(f"Nie udało się oznaczyć doktora o ID {doctor_id} jako usuniętego.")
+
+        cursor.close()
+        connection.close()
+
+    except ValueError:
+        print("Błąd: Podano nieprawidłowe ID.")
+    except Exception as e:
+        print(f"Błąd podczas oznaczania doktora jako usuniętego: {e}")
+
+soft_delete_doctor()
+
